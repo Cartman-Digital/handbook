e90cf18d1bca001be770e2ab33bf0ae835959026
Paavo Pokkinen <paavo.pokkinen@cartman.fi>
2024-09-17 21:07:51 +0300

    test out nix runner

--- flake.nix
+++ flake.nix
@@ -3,8 +3,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-github-actions.url = "github:nix-community/nix-github-actions";
+    nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-github-actions }:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -28,6 +30,17 @@
             venvDir = "venv";
             inherit buildInputs;
           };
+          githubActions = nix-github-actions.lib.mkGithubMatrix { checks = self.packages; };
+
+          packages = rec {
+            mkdocs = pkgs.mkdocs;
+            default = mkdocs;
+          };
+          apps = rec {
+            mkdocs = flake-utils.lib.mkApp { drv = self.packages.${system}.mkdocs; };
+            default = mkdocs;
+          };
         }
       );
+      
 }
